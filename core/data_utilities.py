8be16952b19080662fc733ef2ef9ed560d51817c
gkirgizov <gkirgizov@gmail.com>
2022-11-21 20:43:58 +0300

    tmp fix for missing torch import

--- core/data_utilities.py
+++ core/data_utilities.py
@@ -1,10 +1,14 @@
 from typing import Union
 
 import numpy as np
-import torch as th
+# import torch as th
+
+# tmp to avoid torh install
+th = np
+Tensor = np.ndarray
 
 
-def index_select(tensor: th.Tensor, indices: th.Tensor) -> th.Tensor:
+def index_select(tensor: Tensor, indices: Tensor) -> Tensor:
     ndims = len(tensor.shape)
     assert len(indices.shape) == 2
     assert indices.shape[1] == ndims
@@ -19,17 +23,17 @@ def np_mask_duplicates(a: np.ndarray, axis: int = 0) -> np.ndarray:
     return mask
 
 
-def th_mask_duplicates(a: th.Tensor, axis: int = 0) -> th.Tensor:
+def th_mask_duplicates(a: Tensor, axis: int = 0) -> th.Tensor:
     # TODO: test
     mask = th.zeros(a.shape[axis], dtype=bool)
     mask[th.unique(a, return_inverse=True, dim=axis)[1]] = True
     return mask
 
 
-def mask_duplicates(a: Union[np.ndarray, th.Tensor], axis: int = 0) -> Union[np.ndarray, th.Tensor]:
+def mask_duplicates(a: Union[np.ndarray, Tensor], axis: int = 0) -> Union[np.ndarray, th.Tensor]:
     if isinstance(a, np.ndarray):
         return np_mask_duplicates(a, axis)
-    elif isinstance(a, th.Tensor):
+    elif isinstance(a, Tensor):
         return th_mask_duplicates(a, axis)
     else:
         raise TypeError()
